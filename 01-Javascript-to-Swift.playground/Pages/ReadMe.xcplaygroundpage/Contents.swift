b9c4a7bd098ad56da3c8a30b4c6ca722d09116c1
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2024-01-28 20:44:14 -0500

    readme

--- 01-Javascript-to-Swift.playground/Pages/ReadMe.xcplaygroundpage/Contents.swift
+++ 01-Javascript-to-Swift.playground/Pages/ReadMe.xcplaygroundpage/Contents.swift
@@ -2,3 +2,9 @@
 
 // What Javascript code looks like in Swift
 
+// Examples of swift equivalents for javascript
+
+// swift and javascript are based on C syntax so much is similar
+// variables: let / var
+// loops: for / while
+// values: strings / numbers / arrays
